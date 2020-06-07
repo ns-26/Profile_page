9d3e17367ae045c047b6f449832beff961ae1dfe
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-06-07 09:26:49 +0530

    project done

---
+++ codeforces.js
@@ -0,0 +1,12 @@
+var obj;
+fetch("https://codeforces.com/api/user.info?handles=navdeepsingh.26.2000")
+.then(response => response.json())
+  .then(function(data){ 
+    let result=data.result[0];
+    var handle=result.handle;
+    var rating=result.rating;
+    var rank=result.rank;
+    $(".handle").html("Handle:"+String(handle));
+    $(".rating").html("Rating:"+String(rating));
+    $(".rank").html("Rank:"+String(rank));
+  })
